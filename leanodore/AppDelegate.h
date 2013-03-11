e57e71660dba48df7e3e4f541db4ffa9741ebd96
stetro <stetro.freaky@googlemail.com>
2013-03-11 21:19:51 +0100

    Initial Commit

---
+++ leanodore/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  leanodore
+//
+//  Created by Steffen Tröster on 11.03.13.
+//  Copyright (c) 2013 Steffen Tröster. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ leanodore/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  leanodore
+//
+//  Created by Steffen Tröster on 11.03.13.
+//  Copyright (c) 2013 Steffen Tröster. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
